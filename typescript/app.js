e71eadbade8f74892c67c12c18bf9cca11e7a526
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2023-02-10 12:06:22 -0300

    primeiro teste de tipagem

---
+++ typescript/app.js
@@ -0,0 +1,4 @@
+var soma = function (x, y) {
+    return x + y;
+};
+console.log(soma(9, 8));
---
+++ typescript/app.ts
@@ -0,0 +1,8 @@
+const soma = (x: number, y: number) => {
+    return x + y;
+}
+
+
+
+
+console.log(soma(9, 8))
