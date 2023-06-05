d784a9a18392d9bbe90a307f6dc1642f7479870d
Alex Yudi <94754085+alex-yudi@users.noreply.github.com>
2023-06-05 08:33:57 -0300

    Aula 13 - Repetições for

---
+++ python/aula13-repeticao-for.py
@@ -0,0 +1,15 @@
+'''
+Existem diversas estruturas de repetição dentro do Python.
+
+Uma delas é o "for com contador"
+    for c in range(inicio, fim):
+        código_para_repetir
+    obs.: O 'c' poderia ser substituído por qualquer outra letra ou declaração
+    obs.: Lembrar que o fim SEMPRE é excluído da conta, uma vez que a contagem começa do 0 e ele fica de fora.
+É possível, também, passar um terceiro argumento que informa se devo "pular" alguma quantidade de loops.
+
+
+'''
+
+for teste in range(5):
+    print(teste)
